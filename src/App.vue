5df32f42484a23b3977c12370bc5fd53ba03fa0c
zhaojingyu <810630528@qq.com>
2022-06-03 23:40:08 +0800

    引入了vant2组件，进行了rem适配

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <h1>黑马头条</h1>
+    <i class="iconfont icon-dianzan"></i>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="test">hahah</div>
   </div>
 </template>
 
 <style lang="less">
-
+  .test{
+    width: 200px;
+    height: 200px;
+    background-color: pink;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
+// 导入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 导入适配
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
+Vue.use(Vant)
 new Vue({
   router,
   store,
